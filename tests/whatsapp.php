8d6a918f11db984ba6e38d2a90fbd6b9fbed71f5
Geboybuer <shane.pacht@web.de>
2014-08-26 20:16:27 +0200

    Update whatsapp.php

--- tests/whatsapp.php
+++ tests/whatsapp.php
@@ -14,9 +14,9 @@ function fgets_u($pStdn) {
 	}
 }
 
-$nickname = "WhatsAPI Test";
-$sender =   "34659730605"; // Mobile number with country code (but without + or 00)
-$imei =     "351565053325764"; // MAC Address for iOS IMEI for other platform (Android/etc)
+$nickname = "Michelle Sluka";
+$sender =   "4915770347937"; // Mobile number with country code (but without + or 00)
+$imei =     "11409076712265"; // MAC Address for iOS IMEI for other platform (Android/etc)
 
 $countrycode = substr($sender, 0, 2);
 $phonenumber=substr($sender, 2);
