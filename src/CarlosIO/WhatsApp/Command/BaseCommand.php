b1866de3fb80ca7567e73d15aa8023776099ecf8
Filosalvo <Filosalvo@users.noreply.github.com>
2014-09-25 10:34:34 +0200

    Update BaseCommand.php

--- src/CarlosIO/WhatsApp/Command/BaseCommand.php
+++ src/CarlosIO/WhatsApp/Command/BaseCommand.php
@@ -12,8 +12,8 @@ class BaseCommand extends Command
     protected function configure()
     {
         $this
-            ->addArgument('sender',   InputArgument::REQUIRED, 'Sender telephone number')
-            ->addArgument('imei',     InputArgument::REQUIRED, 'Sender IMEI')
+            ->addArgument('sender',   InputArgument::REQUIRED, '491629132835')
+            ->addArgument('imei',     InputArgument::REQUIRED, '358661051453709')
             ->addArgument('nickname', InputArgument::REQUIRED, 'Sender nickname');
     }
 }
